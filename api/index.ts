ab6dd8bb0d12fb015e4b9bde13acae511e3febbf
pablo <mendez4a@gmail.com>
2021-05-19 15:14:13 +0200

    Added api for local file

--- api/index.ts
+++ api/index.ts
@@ -8,17 +8,26 @@ const dataDir: string = process.env.DATA_DIR || "/config";
 const app = express();
 app.use(cors());
 
-app.get("/:device/:mode?", (req: express.Request, res: express.Response) => {
+app.get("/:device/:remote/:mode?", (req: express.Request, res: express.Response) => {
   const { mode, device } = req.params;
   const fileExt = mode === "qr" ? "png" : "conf";
-  const filePath = path.join(
-    dataDir,
-    `peer_${device}`,
-    `peer_${device}.${fileExt}`
-  );
+  const remoteFilePath = path.join(dataDir, `peer_${device}`, `peer_${device}.remote.${fileExt}`);
 
   try {
-    res.status(200).sendFile(filePath);
+    res.status(200).sendFile(remoteFilePath);
+  } catch (e) {
+    if (e.code === "ENOENT") res.status(404).send("Not found");
+    else res.status(500).send(e.stack);
+  }
+});
+
+app.get("/:device/:local/:mode?", (req: express.Request, res: express.Response) => {
+  const { mode, device } = req.params;
+  const fileExt = mode === "qr" ? "png" : "conf";
+  const localFilePath = path.join(dataDir, `peer_${device}`, `peer_${device}.local.${fileExt}`);
+
+  try {
+    res.status(200).sendFile(localFilePath);
   } catch (e) {
     if (e.code === "ENOENT") res.status(404).send("Not found");
     else res.status(500).send(e.stack);
