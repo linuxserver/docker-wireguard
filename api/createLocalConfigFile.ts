e2d06bcb4db3a47676a540510d6706272638f8db
pablo <mendez4a@gmail.com>
2021-05-20 12:48:34 +0200

    fix bug, added test

--- api/createLocalConfigFile.ts
+++ api/createLocalConfigFile.ts
@@ -1,8 +1,10 @@
 import fs from "fs";
+import got from "got";
+import ipRegex from "ip-regex";
 
 // Params
-const dappnodeApiInternalIp = "my.dappnode/global-envs/INTERNAL_IP";
-const serverPort = "51820";
+const dappnodeApiInternalIp = "http://my.dappnode/global-envs/INTERNAL_IP";
+const serverPort = 51820;
 
 export async function createLocalConfigFile(path: string): Promise<string> {
   try {
@@ -10,26 +12,33 @@ export async function createLocalConfigFile(path: string): Promise<string> {
     const localEndpoint = `${localIp}:${serverPort}`;
 
     const remoteConfigFile = fs.readFileSync(path, "utf8");
-    const localConfigFile = remoteConfigFile
-      .split("\n")
-      .map((row) => (row.startsWith("Endpoint =") ? (row = `Endpoint = ${localEndpoint}`) : row))
-      .join("\n");
+    const localConfigFile = setLocalEndpoint(remoteConfigFile, localEndpoint);
 
     if (localConfigFile === remoteConfigFile) throw Error("Error generating localConfigFile");
     return localConfigFile;
   } catch (e) {
-    throw Error(e);
+    e.message = `Error creating : ${e.message}`;
+    throw e;
   }
 }
 
+// Utils
+
 async function getLocalIp(): Promise<string> {
   try {
-    const response = await fetch(dappnodeApiInternalIp);
-    const localIp = response.text();
+    const localIp = await got(dappnodeApiInternalIp).text();
     if (!localIp) throw Error("localIp is empty");
+    if (!ipRegex({ exact: true }).test(localIp)) throw Error("Invalida localIp");
     return localIp;
   } catch (e) {
-    e.message = "Error fetching localIp" + e.message;
-    throw Error(e);
+    e.message = `Error fetching localIp: ${e.message}`;
+    throw e;
   }
+}
+
+export function setLocalEndpoint(configFile: string, localEndpoint: string): string {
+  return configFile
+    .split("\n")
+    .map((row) => (row.startsWith("Endpoint =") ? `Endpoint = ${localEndpoint}` : row))
+    .join("\n");
 }
