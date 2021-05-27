4125651fcc43061fda73b4050456bd7add32e840
Lion - dapplion <35266934+dapplion@users.noreply.github.com>
2021-05-27 08:40:48 +0200

    Update getWireguardCredentialsRemote.ts

--- api/src/executables/getWireguardCredentialsRemote.ts
+++ api/src/executables/getWireguardCredentialsRemote.ts
@@ -10,21 +10,22 @@ import { params } from "../params";
 import { getRemoteConfigFilePath } from "../createLocalConfigFile";
 import fs from "fs";
 
-// If user does CTRL + C, exit process
-process.on("SIGINT", () => process.exit(128));
-
 (async function (): Promise<void> {
   try {
     console.log(
-      `Fetching remote Wireguard credentials. It may take some time; use CTRL + C to stop`,
+      `Preparing remote Wireguard credentials; use CTRL + C to stop`,
     );
+
     const remoteFilePath = getRemoteConfigFilePath(params.MASTER_ADMIN, "conf");
-    const wireguardRemoteCredentials = fs.readFileSync(remoteFilePath, "utf-8");
+    const config = fs.readFileSync(remoteFilePath, "utf-8");
 
-    // If rendering the QR fails, show the error and continue, the raw URL is consumable
-    console.log(`
+    // If rendering the QR fails, show error and the raw config
+    const qrStr = await renderQrCode(config).catch(e => {
+      console.error("Error rendering QR code", e);
+      return config;
+    })
 
-${await renderQrCode(wireguardRemoteCredentials).catch((e) => e.stack)}`);
+    console.log(`\n\n${qrStr}`);
   } catch (e) {
     // Exit process cleanly to prevent showing 'Unhandled rejection'
     console.error(e);
