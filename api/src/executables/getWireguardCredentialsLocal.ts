9e9d8275d4f1c45169df710e89c4dac6545002ff
Lion - dapplion <35266934+dapplion@users.noreply.github.com>
2021-05-27 08:39:01 +0200

    Update getWireguardCredentialsLocal.ts

--- api/src/executables/getWireguardCredentialsLocal.ts
+++ api/src/executables/getWireguardCredentialsLocal.ts
@@ -9,21 +9,21 @@ import { renderQrCode } from "../renderQrCode";
 import { params } from "../params";
 import { createLocalConfigFile } from "../createLocalConfigFile";
 
-// If user does CTRL + C, exit process
-process.on("SIGINT", () => process.exit(128));
-
 (async function (): Promise<void> {
   try {
     console.log(
-      `Fetching local Wireguard credentials. It may take some time; use CTRL + C to stop`,
+      `Preparing local Wireguard credentials; use CTRL + C to stop`,
     );
 
-    const wireguardLocalCredentials = await createLocalConfigFile(params.MASTER_ADMIN);
+    const config = await createLocalConfigFile(params.MASTER_ADMIN);
+    
+    // If rendering the QR fails, show error and the raw config
+    const qrStr = await renderQrCode(config).catch(e => {
+      console.error("Error rendering QR code", e)
+      return config
+    })
 
-    // If rendering the QR fails, show the error and continue, the raw URL is consumable
-    console.log(`
-
-${await renderQrCode(wireguardLocalCredentials).catch((e) => e.stack)}`);
+    console.log(`\n\n${qrStr}`);
   } catch (e) {
     // Exit process cleanly to prevent showing 'Unhandled rejection'
     console.error(e);
