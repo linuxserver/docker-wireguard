04aa143cda128b7f5646b9295436ed14f2420936
pablo <mendez4a@gmail.com>
2021-05-27 14:32:32 +0200

    Simplified cli

--- api/src/executables/getWireguardCredentials.ts
+++ api/src/executables/getWireguardCredentials.ts
@@ -7,61 +7,22 @@
 
 import { renderQrCode } from "../renderQrCode";
 import { params } from "../params";
-import {
-  createLocalConfigFile,
-  getRemoteConfigFilePath,
-} from "../createLocalConfigFile";
+import { createLocalConfigFile, getRemoteConfigFilePath } from "../createLocalConfigFile";
 import fs from "fs";
 
 (async function (): Promise<void> {
   try {
-    // getWireguardCredentials --local => return wireguard local credentials in plain text
-    // getWireguardCredentials --local --qr => return wireguard local credentials in qr format
-
-    const args = process.argv;
-    if (
-      args.length > 2 ||
-      (args.length > 0 && !args.includes("local" || "qr" || "help"))
-    )
-      throw Error(params.ERROR_MESSAGE);
-
-    // getWireguardCredentials => return wireguard remote credentials in plain text
-    if (args.length === 0) {
-      console.log(createStartingLog("remote", "text"));
-      console.log(await getCredentials("remote", "text"));
+    // Help message
+    if (process.argv.includes("--help")) {
+      console.log(params.HELP_MESSAGE);
+      return;
     }
 
-    switch (args[0]) {
-      // getWireguardCredentials --qr => return wireguard remote credentials in qr format
-      case "--qr":
-        if (args[1] === "local") {
-          console.log(createStartingLog("local", "qr"));
-          console.log(await getCredentials("local", "qr"));
-          return;
-        } else if (!args[1]) {
-          console.log(createStartingLog("remote", "qr"));
-          console.log(await getCredentials("remote", "qr"));
-          return;
-        }
-        throw Error(params.ERROR_MESSAGE);
+    const isQr = process.argv.includes("--qr");
+    const isLocal = process.argv.includes("--local");
 
-      case "--local":
-        if (args[1] === "qr") {
-          console.log(createStartingLog("local", "qr"));
-          console.log(await getCredentials("local", "qr"));
-          return;
-        } else if (!args[1]) {
-          console.log(createStartingLog("local", "text"));
-          console.log(await getCredentials("local", "text"));
-          return;
-        }
-        throw Error(params.ERROR_MESSAGE);
-
-      case "--help":
-        console.log(params.HELP_MESSAGE);
-      default:
-        throw Error(params.ERROR_MESSAGE);
-    }
+    console.log(createStartingLog(isLocal ? "local" : "remote", isQr ? "qr" : "text"));
+    console.log(await getCredentials(isLocal, isQr));
   } catch (e) {
     // Exit process cleanly to prevent showing 'Unhandled rejection'
     console.error(e);
@@ -70,45 +31,35 @@ import fs from "fs";
 })();
 
 // Utils
-function createStartingLog(
-  credentials: "local" | "remote",
-  mode: "qr" | "text"
-): string {
+function createStartingLog(credentials: "local" | "remote", mode: "qr" | "text"): string {
   return `Preparing ${credentials} ${mode} Wireguard credentials; use CTRL + C to stop`;
 }
 
-async function getCredentials(
-  credentials: "local" | "remote",
-  mode: "qr" | "text"
-) {
-  switch (credentials) {
-    case "local":
-      const localTextCreds = await createLocalConfigFile(params.MASTER_ADMIN);
-      if (mode === "qr") return localTextCreds;
+async function getCredentials(isLocal: boolean, isQr: boolean) {
+  // LOCAL
+  if (isLocal) {
+    const localTextCreds = await createLocalConfigFile(params.MASTER_ADMIN);
+    if (!isQr) return localTextCreds;
+    // If rendering the QR fails, show error and the raw config
+    const localQrCreds = await renderQrCode(localTextCreds).catch((e) => {
+      console.error("Error rendering local QR code", e);
+      return localTextCreds;
+    });
 
-      // If rendering the QR fails, show error and the raw config
-      const localQrCreds = await renderQrCode(localTextCreds).catch((e) => {
-        console.error("Error rendering local QR code", e);
-        return localTextCreds;
-      });
-
-      return localQrCreds;
-
-    case "remote":
-      const remoteFilePath = getRemoteConfigFilePath(
-        params.MASTER_ADMIN,
-        "conf"
-      );
-      const remoteTextCreds = fs.readFileSync(remoteFilePath, "utf-8");
-
-      if (mode === "text") return remoteTextCreds;
-
-      // If rendering the QR fails, show error and the raw remoteTextCreds
-      const remoteQrCreds = await renderQrCode(remoteTextCreds).catch((e) => {
-        console.error("Error rendering remote QR code", e);
-        return remoteTextCreds;
-      });
-
-      return remoteQrCreds;
+    return localQrCreds;
   }
+
+  //REMOTE
+  const remoteFilePath = getRemoteConfigFilePath(params.MASTER_ADMIN, "conf");
+  const remoteTextCreds = fs.readFileSync(remoteFilePath, "utf-8");
+
+  if (!isQr) return remoteTextCreds;
+
+  // If rendering the QR fails, show error and the raw remoteTextCreds
+  const remoteQrCreds = await renderQrCode(remoteTextCreds).catch((e) => {
+    console.error("Error rendering remote QR code", e);
+    return remoteTextCreds;
+  });
+
+  return remoteQrCreds;
 }
